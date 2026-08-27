c548ddbddd8cc64b05e96905089c831765b2a1ce
Shristi Singh Astha <144475951+ShristiSinghAstha@users.noreply.github.com>
2026-08-27 21:29:54 +0530

    Time: 0 ms (100%), Space: 14.9 MB (43.03%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,16 +1,30 @@
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         unordered_map<int, int> pairIdx;
+
+//         for (int i = 0; i < nums.size(); ++i) {
+//             int num = nums[i];
+//             if (pairIdx.find(target - num) != pairIdx.end()) {
+//                 return {i, pairIdx[target - num]};
+//             }
+//             pairIdx[num] = i;
+//         }
+
+//         return {};        
+//     }
+// };
 class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> pairIdx;
-
-        for (int i = 0; i < nums.size(); ++i) {
-            int num = nums[i];
-            if (pairIdx.find(target - num) != pairIdx.end()) {
-                return {i, pairIdx[target - num]};
+    public:
+        vector<int> twoSum(vector<int>& nums, int target) {
+            unordered_map<int, int> pairIdx;
+            for (int i = 0; i < nums.size(); ++i) {
+                int num = nums[i];
+                if (pairIdx.find(target - num) != pairIdx.end()) {
+                    return {i, pairIdx[target - num]};
+                }
+                pairIdx[num] = i;
             }
-            pairIdx[num] = i;
+            return {};
         }
-
-        return {};        
-    }
 };
