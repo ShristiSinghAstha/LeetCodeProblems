91c6146b10fc309adfbbdb26e5db67c36c0d2a21
Shristi Singh Astha <144475951+ShristiSinghAstha@users.noreply.github.com>
2026-09-11 21:07:25 +0530

    Time: 1 ms (57.14%), Space: 8.6 MB (35.92%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if (x< 0)
-        return false;
-        
-        int y=x;
-        long long rev=0;
+        if (x<0){
+            return false;
+        }
+        int original = x;
+        long long reverse =0;
 
         while (x>0){
             int digit = x%10;
-            rev = rev*10+digit;
-            x/=10;
+            reverse=reverse*10+digit;
+            x=x/10;
         }
-        return y==rev;
-        };
+        return original == reverse;
+    }
 };
