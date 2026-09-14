71fb14257b5b5752dbf06043f4eb67d334ca94a4
Shristi Singh Astha <144475951+ShristiSinghAstha@users.noreply.github.com>
2026-09-15 00:00:05 +0530

    Time: 0 ms (100%), Space: 14.8 MB (58.66%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,19 +1,3 @@
-// class Solution {
-// public:
-//     vector<int> twoSum(vector<int>& nums, int target) {
-//         unordered_map<int, int> pairIdx;
-
-//         for (int i = 0; i < nums.size(); ++i) {
-//             int num = nums[i];
-//             if (pairIdx.find(target - num) != pairIdx.end()) {
-//                 return {i, pairIdx[target - num]};
-//             }
-//             pairIdx[num] = i;
-//         }
-
-//         return {};        
-//     }
-// };
 class Solution {
     public:
         vector<int> twoSum(vector<int>& nums, int target) {
